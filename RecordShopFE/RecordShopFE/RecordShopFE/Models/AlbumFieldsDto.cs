1010cb6f43f06b3ec6d33236f80f4baa8a6385ef
termgiles <Thomas.giles2222@gmail.com>
2025-01-30 19:24:26 +0000

    feat: post albums

---
+++ RecordShopFE/RecordShopFE/RecordShopFE/Models/AlbumFieldsDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecordShopFE.Models
+{
+    public class AlbumFieldsDto
+
+    {
+
+        [Required(ErrorMessage = "Doit entrer un nom")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Doit inscrire un artiste")]
+        public string Artist { get; set; }
+        [Required(ErrorMessage = "Doit entrer dans une annee")]
+        public int Released { get; set; }
+        [Required(ErrorMessage = "Doit entrer dans un gnere")]
+        public string Genre { get; set; }
+        [Required(ErrorMessage = "doit entrer quelques informations")]
+        public string Information { get; set; }
+    }
+}
